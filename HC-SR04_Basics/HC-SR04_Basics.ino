149e954a91894aa689462bd48bd8ec05c6a65641
RozarioCJS <sodienrozario@gmail.com>
2026-03-12 21:58:13 +0200

    added servo motor printing to screen

--- HC-SR04_Basics/HC-SR04_Basics.ino
+++ HC-SR04_Basics/HC-SR04_Basics.ino
@@ -1,14 +1,33 @@
+#include <Servo.h>
+
+Servo myservo;
+
 const int trigPin = 9;  
 const int echoPin = 10; 
+const int servoPin = 8;
 long duration;
 int distance;
 
+
 void setup() {
   pinMode(trigPin, OUTPUT);  
-	pinMode(echoPin, INPUT);  
+	pinMode(echoPin, INPUT);
+  myservo.attach(servoPin);
 	Serial.begin(9600);  
 
 }
+
+void ScanD() {
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  delay(100);
+}
+
+void PrintDistance( int distance) {
+    Serial.print("Distance: ");
+    Serial.println(distance);
+}
+
 
 void loop() {
   digitalWrite(trigPin, LOW);
@@ -16,10 +35,21 @@ void loop() {
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  delay(100);
+  
+  ScanD();
+  PrintDistance(distance);
+  for (int pos = 0; pos <= 180; pos +=1) {
+    myservo.write(pos);
+    Serial.print("Pos: ");
+    Serial.println(pos);
+    delay(15);
+  }
+  PrintDistance(distance);
+ for (int pos = 180; pos >= 0; pos -=1) {
+    myservo.write(pos);
+    Serial.print("Pos: ");
+    Serial.println(pos);
+    delay(15);
+ }
+  
 }
