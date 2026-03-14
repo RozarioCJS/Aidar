ae1154df6dc3d20a14366bb64ab995c25e490c6c
RozarioCJS <sodienrozario@gmail.com>
2026-03-14 09:15:59 +0200

    was not paying attention to when the pulse was sent, so i would sent the pulse once then just read the whole time for it but now it sends and reads the pulse within the for loop so the distance is update more freq

--- HC-SR04_Basics/HC-SR04_Basics.ino
+++ HC-SR04_Basics/HC-SR04_Basics.ino
@@ -18,33 +18,31 @@ void setup() {
 }
 
 void ScanD() {
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration * 0.034 / 2;
-  delay(100);
-}
-
-
-void loop() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  
-  ScanD();
-  for (int pos = 0; pos <= 180; pos +=1) {
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  //delay(100);
+}
+
+
+void loop() {
+ for (int pos = 0; pos <= 180; pos +=1) {
     myservo.write(pos);
+    ScanD();
     Serial.print(pos);
     Serial.print(",");
-    Serial.println(distance);   
-    delay(15);
-  }
+    Serial.println(distance);
+ }
  for (int pos = 180; pos >= 0; pos -=1) {
     myservo.write(pos);
+    ScanD();
     Serial.print(pos);
     Serial.print(",");
-    Serial.println(distance);   
-    delay(15);
+    Serial.println(distance); 
  }
   
 }
